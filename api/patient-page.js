2e77dd106e3919da9fd8b0bbda354b53a6276646
Joel Raitt <joelraitt@gmail.com>
2026-03-04 12:30:27 -0800

    fix: runtime nodejs

--- api/patient-page.js
+++ api/patient-page.js
@@ -1,4 +1,4 @@
-export const config = { runtime: 'nodejs18.x' };
+export const config = { runtime: 'nodejs' };
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const REPO = 'imperialinteractive/thrive-practice';
